95da9f0d8815b2fbd0c98320a07187627f0e02e8
wangyidi0731 <wangyiidii@163.com>
2021-09-26 23:05:51 +0800

    api

--- config/env.js
+++ config/env.js
@@ -1,14 +1,11 @@
 //根据不同的环境更改不同的baseUrl
-export let baseUrl = "";
-export let ipPort = "";
+export let baseUrl = "/api";
+export let ipPort;
 
 if (process.env.NODE_ENV == "development") {
-  baseUrl = "/api";
   ipPort = "127.0.0.1:7880";
 } else if (process.env.NODE_ENV == "production") {
-  baseUrl = "http://lab.yiidii.cn:1170";
   ipPort = "159.75.42.178:7880";
 } else {
-  baseUrl = "http://lab.yiidii.cn:1170";
   ipPort = "159.75.42.178:7880";
 }
